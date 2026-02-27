ca9df6f867dfb45863a25b1a74384c555b9e861d
storoo <storoo@users.noreply.github.com>
2026-02-27 17:44:10 +0000

    deploy: 0ce047758c29c75e7a61f5b0f088f429acca620b

---
+++ _next/static/chunks/app/etc/page-5ec70303fb219816.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[339],{5120:(e,s,n)=>{Promise.resolve().then(n.bind(n,8452)),Promise.resolve().then(n.bind(n,3365))}},e=>{var s=s=>e(e.s=s);e.O(0,[711,330,637,860,441,684,358],()=>s(5120)),_N_E=e.O()}]);
